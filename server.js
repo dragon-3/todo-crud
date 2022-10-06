2644844a71836d917b1545b7c4253990a2d0bc30
Sean Narron <seannarron9@gmail.com>
2022-10-06 13:25:35 -0700

    second commit on heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('server/db.json');
+const router = jsonServer.router('./db.json');
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
