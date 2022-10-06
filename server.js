b3d17174e992b686076d84a88b3f51622d192216
Sean Narron <seannarron9@gmail.com>
2022-10-06 13:11:59 -0700

    first commit on heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('server/db.json');
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+)
+
+server.use(router);
+server.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+});
