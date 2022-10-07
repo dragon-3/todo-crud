6473e1108f98b9ac820c5956830f7081a0b0ac63
Sean Narron <seannarron9@gmail.com>
2022-10-06 18:35:04 -0700

    mod

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/home' element={<Home />}  />
+          <Route path='/' element={<Home />}  />
           <Route path=':id' element={<Edit />}  />
 
         </Routes>
